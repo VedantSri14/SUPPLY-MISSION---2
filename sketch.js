a52ab1aa87022dcef7902abcd98b642f0578ca29
VedantSri14 <71330279+VedantSri14@users.noreply.github.com>
2020-10-10 23:20:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var helicopterIMG, helicopterSprite, packageSprite, packageIMG;
 var packageBody, ground;
 var block1, block2, block3;
+var world, engine;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -13,8 +14,8 @@ function preload() {
 
 function setup() {
 	createCanvas(800, 700);
-	rectMode(CENTER);
-
+	engine = Engine.create();
+	world = engine.world;
 
 	packageSprite = createSprite(width / 2, 80, 10, 10);
 	packageSprite.addImage(packageIMG)
@@ -24,49 +25,37 @@ function setup() {
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale = 0.6
 
-	//block1 = createSprite(290, 610, 20, 100);
 	block1 = Bodies.rectangle(290, 610, 20, 100, { isStatic: true, restitution: 0.3 });
-	block1.shapeColor = "red";
-	World.add(World, block1)
-
-	//block2 = createSprite(400, 650, 200, 20);
+	World.add(world, block1)
+	
 	block2 = Bodies.rectangle(400, 650, 200, 20, { isStatic: true, restitution: 0.3 });
-	block2.shapeColor = "red";
-	World.add(World, block2)
+	World.add(world, block2)
 
-	//block3 = createSprite(500, 610, 20, 100);
 	block3 = Bodies.rectangle(500, 610, 20, 100, { isStatic: true, restitution: 0.3 });
-	block3.shapeColor = "red";
-	World.add(World, block3)
+	World.add(world, block3)
 
 	groundSprite = createSprite(width / 2, height - 35, width, 10);
 	groundSprite.shapeColor = color(255)
 
-	engine = Engine.create();
-	world = engine.world;
-
 	packageBody = Bodies.circle(width / 2, 200, 5, { restitution: 0.3, isStatic: true });
-	World.add(World, packageBody);
-
-	//block1 = Bodies.rectangle;
-	//World.add(world, block1);
-
-	//block2 = Bodies.rectangle;
-	///World.add(world, block2);
-
-	//block3 = Bodies.rectangle;
-	//World.add(world, block3);
+	World.add(world, packageBody);
 
 	ground = Bodies.rectangle(width / 2, 650, width, 10, { isStatic: true });
-	World.add(World, ground);
-
-	Engine.run(engine);
+	World.add(world, ground);
+	
+		Engine.run(engine);
 
 }
 
 function draw() {
-	rectMode(CENTER);
 	background(0);
+	 
+	rectMode(CENTER);
+	rect(block1.position.x, block1.position.y, 20, 100);
+	rectMode(CENTER);
+	rect(block2.position.x, block2.position.y, 200, 20);
+	rectMode(CENTER);
+	rect(block3.position.x, block3.position.y, 20, 100);
 
 	packageSprite.x = packageBody.position.x
 	packageSprite.y = packageBody.position.y
@@ -78,5 +67,7 @@ function draw() {
 function keyPressed() {
 	if (keyCode === DOWN_ARROW) {
 		Matter.Body.setStatic(packageBody, false);
+		
+
 	}
 }
